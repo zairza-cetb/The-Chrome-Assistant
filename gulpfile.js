ac9a1221c99621b61ba5aa4cfd9a25d0feac9663
Harkishen-Singh <harkishensingh@hotmail.com>
2019-12-04 23:07:39 +0530

    added webpack compiler, prettier
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp'),
     del = require('del'),
     seqRunner = require('run-sequence'),
     uglify = require('gulp-uglify-es').default,
+    webpack = require('webpack-stream'),
+    compiler = require('webpack'),
+    rename = require('gulp-rename'),
+    prettier = require('gulp-prettier'),
     minifyHTML = require('gulp-htmlmin')
 
 gulp.task('clean', () => {
@@ -17,13 +21,24 @@ gulp.task('default', () => {
 
 gulp.task('pack-dependencies', () => {
     return gulp.src('./src/assets/js/*')
-    // .pipe(uglify())
     .pipe(gulp.dest('./pkg/assets/js'));
+});
+
+gulp.task('js-pretty', () => {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(prettier({
+            singleQuote: true
+        }))
+        .pipe(gulp.dest('./src/scripts'));
 });
 
 gulp.task('bundle-scripts', () => {
     return gulp.src('./src/scripts/**/*.js')
+    .pipe(webpack({
+        mode: 'development'
+    }, compiler))
     .pipe(uglify())
+    .pipe(rename('assistant.js'))
     .pipe(gulp.dest('./pkg/scripts'));
 });
 
@@ -41,7 +56,7 @@ gulp.task('bundle-views', () => {
 });
 
 gulp.task('build', () => {
-    seqRunner('clean', ['pack-dependencies', 'bundle-scripts', 'bundle-views', 'bundle-remain']);
+    seqRunner('clean', 'js-pretty', ['pack-dependencies', 'bundle-scripts', 'bundle-views', 'bundle-remain']);
 });
 
 gulp.task('watch', ['package-dependencies'], () => {
